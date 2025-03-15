36358fb5b78dc76b7471b431c2fceef6d7c5518e
shaunakc11 <helloshaunak276@gmail.com>
2025-03-15 22:08:54 +0530

    unused imports

--- src/handler/auth.handler.ts
+++ src/handler/auth.handler.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
 import db from '../db/db';
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
