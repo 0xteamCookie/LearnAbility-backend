1480764e5c92efcb59d4ac7b408336450a5f583a
0xPixelNinja <rkrabha1858@gmail.com>
2025-03-17 01:58:32 +0530

    fixed upload bug

--- src/routes/pyos.routes.ts
+++ src/routes/pyos.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/auth.middleware';
+import { upload } from '../middleware/upload.middleware';
 import * as subjectHandler from '../handler/subject.handler';
 import * as topicHandler from '../handler/topic.handler';
 import * as tagHandler from '../handler/tag.handler';
+import * as sourceHandler from '../handler/source.handler';
 
 const router = Router();
 
@@ -19,5 +21,11 @@ router.delete('/topics/:id', topicHandler.deleteTopic);
 router.get('/tags', tagHandler.getAllTags);
 router.post('/tags', tagHandler.createTag);
 router.delete('/tags/:id', tagHandler.deleteTag);
+
+// Material routes using DataSource handler (Not that necessary)
+router.get('/materials', sourceHandler.getAllDataSources);
+router.get('/materials/:id', sourceHandler.getDataSourceById);
+router.delete('/materials/:id', sourceHandler.deleteDataSource);
+router.post('/materials', upload.array('file', 5), sourceHandler.createDataSource);
 
 export { router as pyosRoutes };
