65e54a31fb8b57a622792878ae9d86c0a52c1aea
shaunakc11 <helloshaunak276@gmail.com>
2025-03-18 12:01:28 +0530

    packages

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getProfile, login, register } from '../handler/auth.handler';
+import { getProfile, login, logout, register } from '../handler/auth.handler';
 import { authenticate } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', authenticate, logout);
 router.get('/me', authenticate, getProfile);
 
 export { router as authRoutes };
